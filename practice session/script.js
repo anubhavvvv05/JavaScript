f51847352973dabc3478e6b5720509c94053295a
Anubhav Mishra <anumis1806726@gmail.com>
2026-04-10 14:55:50 +0530

    DOM

--- practice session/script.js
+++ practice session/script.js
@@ -1,91 +1,91 @@
-// //1
+//1
 
-// function sayHello(){
-//     console.log("hello JavaScript");
-// }
+function sayHello(){
+    console.log("hello JavaScript");
+}
 
-// sayHello();
-
-
-// //2
+sayHello();
 
 
-// function add(a, b) {
-
-//     return a + b;
-// }
-// let ans = add(5, 6);
-// console.log(ans);
+//2
 
 
+function add(a, b) {
+
+    return a + b;
+}
+let ans = add(5, 6);
+console.log(ans);
 
 
-// //3
-
-// function greet(guest = "Guest") {
-//     console.log("Hii " + guest);
-// }
 
 
-// greet();
+//3
+
+function greet(guest = "Guest") {
+    console.log("Hii " + guest);
+}
 
 
-// //4.1
+greet();
 
 
-// function addUnlimited(...nums) {
-//     let sum = 0;
-//     nums.forEach(val => {
-//         sum = sum + val;
-//     });
-//     console.log(sum);
-// }
-
-// addUnlimited(1,2,3,4,5);
-
-// //4.2
+//4.1
 
 
-// function addUnlimited(...num){
-//     let sum = 0 ;
-//     for (let i = 0; i < num.length; i++) {
-//        sum = sum + num[i] ;
+function addUnlimited(...nums) {
+    let sum = 0;
+    nums.forEach(val => {
+        sum = sum + val;
+    });
+    console.log(sum);
+}
 
-//     }
-//     console.log(sum);
-// }
+addUnlimited(1,2,3,4,5);
 
-// addUnlimited(1,2,3,4,5,6);
-
-
-// //4.3
-
-// function addUnlimited(...num){
-//     let ans = num.reduce(function(acc, val){
-//         return acc+ val;
-//     } , 0 );
-//     console.log(ans);
-// }
-// addUnlimited(1,2,3,4,5,6,7);
+//4.2
 
 
-// (function(){
-//     console.log("I run instantly!")
+function addUnlimited(...num){
+    let sum = 0 ;
+    for (let i = 0; i < num.length; i++) {
+       sum = sum + num[i] ;
 
-// })();
+    }
+    console.log(sum);
+}
+
+addUnlimited(1,2,3,4,5,6);
 
 
-// //5
+//4.3
 
-// let person = {
-//     Name : "Anubhav",
-//     Age: 22,
-//     City: "Sultanpur",
-// }
+function addUnlimited(...num){
+    let ans = num.reduce(function(acc, val){
+        return acc+ val;
+    } , 0 );
+    console.log(ans);
+}
+addUnlimited(1,2,3,4,5,6,7);
 
-// for (let key in person) {
-//     console.log(key + " : " + person[key])
-// }
+
+(function(){
+    console.log("I run instantly!")
+
+})();
+
+
+//5
+
+let person = {
+    Name : "Anubhav",
+    Age: 22,
+    City: "Sultanpur",
+}
+
+for (let key in person) {
+    console.log(key + " : " + person[key])
+}
 
 
 //6
