2f608622294901f890c70d37bae3d8d095d02579
Anubhav Mishra <anumis1806726@gmail.com>
2026-04-10 14:58:23 +0530

    loop practice

--- loop2/script.js
+++ loop2/script.js
@@ -1,24 +1,24 @@
 
 //Print multiplication table of 5Use loop to print 5 × 1 to 5 × 10.
 
-// for (let i = 1; i <= 10; i++) {
-//     console.log(`5 * ${i} = ${5 * i}`)
+for (let i = 1; i <= 10; i++) {
+    console.log(`5 * ${i} = ${5 * i}`)
 
-// }
+}
 
 
 
 //Count how many numbers between 1 and 15 are greater than 8 Loop and count conditionally.
 
 
-// let count = 0;
-// for (let i = 1; i <= 15; i++) {
-//     if (i>8) {
-//         count++;
-//     }
+let count = 0;
+for (let i = 1; i <= 15; i++) {
+    if (i>8) {
+        count++;
+    }
 
 
-// }
+}
 
 // console.log(`${count}`)
 
@@ -27,50 +27,50 @@
 
 // Allow only 3 attempts to enter correct password If user gets it right early, stop. If not → “Account locked”
 
-// let flag = false
-// let attempts = 0;
-// let password = "anubhav";
-// let pass = prompt("password bta...");
-// attempts++;
-// if (password===pass) {
-//      flag=true;
-// }
+let flag = false
+let attempts = 0;
+let password = "anubhav";
+let pass = prompt("password bta...");
+attempts++;
+if (password===pass) {
+     flag=true;
+}
 
-// while(password !== pass){
+while(password !== pass){
 
-//     if (attempts ===3){
-//         console.error("account locked.");
-//         break;
-//     }
-//     pass = prompt("password bta...");
-//     if (password===pass) {
-//     console.log("password khul gya."); flag=true;
-// }
-//     attempts++;
-// }
+    if (attempts ===3){
+        console.error("account locked.");
+        break;
+    }
+    pass = prompt("password bta...");
+    if (password===pass) {
+    console.log("password khul gya."); flag=true;
+}
+    attempts++;
+}
 
-// if (flag===true) {
-//     console.log("account opened");
-// }
+if (flag===true) {
+    console.log("account opened");
+}
 
 
 
 //18. Ask user 5 numbers. Count how many are positive Use loop + condition + counter.
 
 
-// let totalNumbers = 0;
-// let PositiveNumbers = 0;
+let totalNumbers = 0;
+let PositiveNumbers = 0;
 
 
 
-// for (let i = 1; i <= 5; i++) {
-//    number = +prompt("enter number."); 
-//    if (number>0) {
-//     PositiveNumbers++;
-//    }
-// }
+for (let i = 1; i <= 5; i++) {
+   number = +prompt("enter number."); 
+   if (number>0) {
+    PositiveNumbers++;
+   }
+}
 
-// console.log(`total positive numbers ${PositiveNumbers}`);
+console.log(`total positive numbers ${PositiveNumbers}`);
 
 
 
