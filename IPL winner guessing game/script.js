91b69bf1b2f5f668f66d9d9007d8221eec0811b9
Anubhav Mishra <anumis1806726@gmail.com>
2026-04-11 15:49:30 +0530

    IPL winner guessing color game

---
+++ IPL winner guessing game/script.js
@@ -0,0 +1,76 @@
+var arr = [{
+    Team: 'CSK',
+    primary: 'yellow',
+    secondary: 'royalblue',
+    Trophies: 5,
+    FullName: 'Chennai Super Kings',
+    Captain: 'Ruturaj Gaikwad',
+},
+{
+    Team: 'RCB',
+    primary: 'red',
+    secondary: 'gold',
+    Trophies: 1,
+    FullName: 'Royal challengers bengluru',
+    Captain: 'Rajat Patidar',
+},
+{
+    Team: 'MI',
+    primary: 'royalblue',
+    secondary: 'gold',
+    Trophies: 5,
+    FullName: 'Mumbai Indians',
+    Captain: 'Hardik Pandya',
+}, 
+{
+    Team:'KKR',
+    primary:'purple',
+    secondary:'gold',
+    Trophies: 3,
+    FullName:'Kolkata Knight Riders',
+    Captain:'Ajinkya Rahane',
+},
+ {
+    Team:'SRH',
+    primary:'orange',
+    secondary:'black',
+    Trophies: 1,
+    FullName:'sunrisers Hyderabad',
+    Captain:'Ishan Kishan',
+}, 
+{
+    Team:'PBKS',
+    primary:'red',
+    secondary:'blue',
+    Trophies: 0,
+    FullName: 'punjab kings',
+    Captain:'Rishabh Pant',
+}, 
+]
+
+
+
+
+var btn = document.querySelector('button');
+var h1 = document.querySelector('h1')
+var main = document.querySelector('main')
+var div = document.querySelector('div')
+
+btn.addEventListener('click', function () {
+    var winner = arr[Math.floor(Math.random()*arr.length)];
+
+    h1.innerHTML = `
+    <h5>${winner.FullName}</h5>
+    <p><b>Team:</b> ${winner.Team}</p>
+    <p><b>Captain:</b> ${winner.Captain}</p>
+    <p><b>Trophies:</b> ${winner.Trophies}</p>
+    
+`;
+     h1.style.color = winner.primary;
+
+    main.style.backgroundColor = winner.primary
+    div.style.backgroundColor = winner.secondary
+
+    
+
+})
